d4982fda2686b033a43287d04308938f1ee5d7f3
ha-andrew <34655105+ha-andrew@users.noreply.github.com>
2019-12-31 03:29:21 -0500

    Fixed import issues/crahses

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -17,7 +17,6 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import Button from '@material-ui/core/Button';
 import LoginButton from './login/loginButton';
 
 import './app.css'
